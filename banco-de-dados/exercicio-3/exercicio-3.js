8cfeacd1e6a63d7a7e272c74616a65d44bc21bbd
Thiago Vitor Lorena <thiago.vitor.2067@gmail.com>
2025-11-08 09:30:50 -0300

    :sparkles: feat: Adiciona exercício 3 com estrutura completa

---
+++ banco-de-dados/exercicio-3/exercicio-3.js
@@ -0,0 +1,23 @@
+id = {
+  "titulo": "Primeiro Post em MongoDB",
+  "autor": "Thiago Lorena",
+  "conteudo": "Orion Bootcamp",
+  "data_criacao": {
+    "$date": "2025-11-08T12:15:00.000Z"
+  }
+}
+
+
+id_tags = {
+  "titulo": "Explorando Schema Flexível",
+  "autor": "Thiago Lorena",
+  "conteudo": "Orion Bootcamp, Dev!",
+  "data_criacao": {
+    "$date": "2025-11-08T12:16:00.000Z"
+  },
+  "tags": [
+    "nosql",
+    "mongodb",
+    "flexivel"
+  ]
+}
